830887d0fcaff6bf009d6d34de485db1c412f14b
Kenn <johnkenneth.ornias@gmail.com>
2025-02-12 11:53:31 +0800

    feat: add type attribute to User model and migration
    and function to check if type is admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
@@ -63,5 +64,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->type === 'admin';
     }
 }
