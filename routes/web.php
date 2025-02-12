814b0ed07c832934dac1e1d57c933c2104da0e13
Kenn <johnkenneth.ornias@gmail.com>
2025-02-12 12:07:48 +0800

    feat: implement middleware EnsureIsAdmin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\VehicleController;
+use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,9 +20,6 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
 
     Route::get('/home', function () {
         return Inertia::render('Home');
@@ -33,5 +31,5 @@ Route::middleware([
     ], function () {
         Route::resource('/vehicles', VehicleController::class);
         Route::put('/vehicles/{id}/{status}', [VehicleController::class, 'updateStatus'])->name('vehicles.update-status');
-    });
+    })->middleware(EnsureIsAdmin::class);
 });
